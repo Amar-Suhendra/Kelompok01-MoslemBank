e627e898ad06753d532d18ae09b467617a087bed
Rudy Hidayat <m_rudy@mhs.unsyiah.ac.id>
2022-04-20 16:05:50 -0700

    Add Profile

---
+++ profil.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+
+    <title>MoslemBank | Profil</title>
+    <link rel="stylesheet" href="Assets/costum/CSS/style.css" />
+    <!-- Custom fonts for this template-->
+    <link
+      href="vendor/fontawesome-free/css/all.min.css"
+      rel="stylesheet"
+      type="text/css"
+    />
+    <link
+      href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+      rel="stylesheet"
+    />
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.css" rel="stylesheet" />
+  </head>
+  <body class="profil-body">
+      <div class="layout">
+          <div class="img"></div>
+          <h1>Masjid Ar-Rahman</h1>
+          <div class="text">Testing from <strong>Banda Aceh, Indonesia</strong></div>
+          <p>Ini adalah mesjid kita, mau sedekah hubungi saya</p>
+      </div>
+  </body>
+</html>
